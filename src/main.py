62aa3f2633dfe8bde4788292a17469e606502440
dushechka <suskaman@mail.ru>
2026-05-21 22:04:06 +0200

    update 0.1 services module

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import logging
-import pprint
 
+from services import investment_bank
 from utils import get_data_from_excel
 
 from configurate.logging_config import setup_logging
-from src.views import get_date_for_greeting, get_exchange_rate, json_home_page
+from src.views import json_home_page
+from src.services import get_categories_with_profitable_cashback
 
 # create logger
 main_logger = logging.getLogger("main")
@@ -13,14 +14,12 @@ def main() -> None:
     """The program's main function. Responsible for initializing the application,
     parsing command-line arguments,and starting the program's main loop."""
 
-    # print(get_data_from_excel("../data/operations.xlsx"))
-    # print(data)
-    #
-    # date = get_date_for_greeting()
-    # print(date)
-    date ='21.12.2021 16:44:00'
-    data = json_home_page(date)
-    print(data)
+    # date ='21.12.2021 16:44:00'
+    # data = json_home_page(date)
+    data = get_data_from_excel('../data/operations.xlsx')
+    # print(get_categories_with_profitable_cashback(data, '2021', '02'))
+    print(investment_bank('2021-02', data, 10))
+
     return None
 
 
