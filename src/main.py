676ef0209cdd8d397eca4dd2d624ee1b1b948c90
dushechka <suskaman@mail.ru>
2026-05-22 19:54:14 +0200

    add logging and check for modules

--- src/main.py
+++ src/main.py
@@ -1,26 +1,20 @@
 import logging
-import pprint
-
-from utils import get_data_from_excel
 
 from configurate.logging_config import setup_logging
-from src.views import get_date_for_greeting, get_exchange_rate, json_home_page
+from src.views import json_home_page
+from utils import get_data_from_excel
 
 # create logger
 main_logger = logging.getLogger("main")
 
+
 def main() -> None:
     """The program's main function. Responsible for initializing the application,
     parsing command-line arguments,and starting the program's main loop."""
-
-    # print(get_data_from_excel("../data/operations.xlsx"))
-    # print(data)
-    #
-    # date = get_date_for_greeting()
-    # print(date)
-    date ='21.12.2021 16:44:00'
-    data = json_home_page(date)
-    print(data)
+    operations = get_data_from_excel('../data/operations.xlsx')
+    date = "21.12.2021 16:44:00"
+    # data = json_home_page(date)
+    print(operations[:5])
     return None
 
 
